dccaf4b2c5160c2807ebde205d7879164fe5b340
wonyoung2323 <72545216+wonyoung2323@users.noreply.github.com>
2024-10-03 20:19:23 +0900

    Title: 포탑 부수기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241003/포탑 부수기/destroy-the-turret.py
@@ -0,0 +1,137 @@
+from collections import deque
+import copy
+
+def make_list(n, m, arr):
+    attack_list = []
+    for i in range(n):
+        for j in range(m):
+            attack_list.append([arr[i][j], 0, i + j, j])
+
+    return attack_list
+            
+def select_attack1(attack_list):
+    now = attack_list
+    now.sort(key=lambda x : (x[0], -x[1], -x[2], -x[3]))
+
+    for al in attack_list:
+        if al[0] <= 0: 
+            continue
+        else:
+            return al
+
+def select_attack2(attack_list):
+    now = attack_list
+    now.sort(key=lambda x : (-x[0], x[1], x[2], x[3]))
+    return now[0]
+
+def razor(n, m, arr, pos1, pos2, s):
+    dir = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+    q = deque()
+    path = []
+    path.append(pos1)
+    q.append(path)
+    
+    visited = [[False] * m for _ in range(n)]
+    visited[pos1[0]][pos1[1]] = True
+
+    while q:
+        now = q.popleft()
+        now_attack = now[-1]
+
+        for i in range(4):
+            cpy = now.copy()
+            nx = now_attack[0] + dir[i][0]
+            ny = now_attack[1] + dir[i][1]
+
+            if nx < 0:
+                nx = n - 1
+            if ny < 0:
+                ny = m - 1
+            if nx >= n:
+                nx = 0
+            if ny >= m:
+                ny = 0
+
+            if arr[nx][ny] <= 0 or visited[nx][ny]:
+                continue
+            if [nx, ny] == pos2:
+                arr[nx][ny] -= s
+                for p in range(1, len(cpy)):
+                    ppos = cpy[p]
+                    arr[ppos[0]][ppos[1]] -= (s // 2)
+
+                for ii in range(n):
+                    for jj in range(m):
+                        if [ii, jj] not in cpy and [ii, jj] != pos2 and arr[ii][jj] > 0:
+                            arr[ii][jj] += 1
+
+                return arr
+
+            cpy.append([nx, ny])
+            visited[nx][ny] = True
+            q.append(cpy)
+    
+    bomb(n, m, arr, pos1, pos2, s)
+
+def bomb(n, m, arr, pos1, pos2, s):
+    path = []
+    path.append(pos1)
+    dir = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1), (0, 1),
+        (1, -1), (1, 0), (1, 1)
+    ]
+
+    for i in range(8):
+        nx = pos2[0] + dir[i][0]
+        ny = pos2[1] + dir[i][1]
+
+        if nx < 0:
+            nx = n - 1
+        if ny < 0:
+            ny = m - 1
+        if nx >= n:
+            nx = 0
+        if ny >= m:
+            ny = 0
+
+        if [nx, ny] != pos1 and arr[nx][ny] > 0:
+            arr[nx][ny] -= (s // 2)
+            path.append([nx, ny])
+
+        arr[pos2[0]][pos2[1]] -= select_attack1
+
+    for i in range(n):
+        for j in range(m):
+            if [i, j] not in path and [i, j] != pos2 and arr[i][j] > 0:
+                arr[i][j] += 1
+
+    return arr
+
+
+n, m, k = map(int, input().split())
+
+arr = []
+# 공격력, 공격 시점, 행과 열 합, 열 값
+attack_list = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+for i in range(k):
+    attack_list = copy.deepcopy(make_list(n, m, arr))
+
+    weak = select_attack1(attack_list)
+    pos1 = [weak[2] - weak[3], weak[3]]
+    strong = select_attack2(attack_list)
+    pos2 = [strong[2] - strong[3], strong[3]]
+    
+    arr[pos1[0]][pos1[1]] += (n + m)
+
+    arr = copy.deepcopy(razor(n, m, arr, pos1, pos2, arr[pos1[0]][pos1[1]]))
+
+ans = 0
+for i in range(n):
+    for j in range(m):
+        ans = max(ans, arr[i][j])
+
+print(ans)
